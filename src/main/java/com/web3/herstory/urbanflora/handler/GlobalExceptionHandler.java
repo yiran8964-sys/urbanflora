f25f1cb5fb5dc126c66371e0b45c8e06f380158d
yiran8964-sys <yiran8964@gmail.com>
2026-05-06 01:17:34 +0800

    处理上传文件时，返回413 Request Entity Too Large的问题

--- src/main/java/com/web3/herstory/urbanflora/handler/GlobalExceptionHandler.java
+++ src/main/java/com/web3/herstory/urbanflora/handler/GlobalExceptionHandler.java
@@ -3,9 +3,13 @@ package com.web3.herstory.urbanflora.handler;
 import com.web3.herstory.urbanflora.exception.BusinessException;
 import com.web3.herstory.urbanflora.exception.WalletNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
 
 import java.util.stream.Collectors;
 
@@ -33,6 +37,20 @@ public class GlobalExceptionHandler {
                 .map(err -> err.getField() + ": " + err.getDefaultMessage())
                 .collect(Collectors.joining("; "));
         return Result.error(Result.PARAM_ERROR, msg);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Result<?>> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException e) {
+        log.warn("上传文件过大", e);
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(Result.error(Result.PARAM_ERROR, "上传文件过大，请压缩后重试。单文件最大 50MB，请求总大小最大 60MB。"));
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<Result<?>> handleMultipartException(MultipartException e) {
+        log.warn("文件上传失败", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Result.error(Result.PARAM_ERROR, "文件上传失败，请检查文件大小和请求格式。"));
     }
 
     // 处理所有未知异常
